2678184c8c0f9de1dc00d4723ed0483f7b1ee7dc
Nir Tamir <nirtamir2@gmail.com>
2019-04-25 21:30:29 +0300

    HomePage - change text

--- src/ui-components/HomePage.tsx
+++ src/ui-components/HomePage.tsx
@@ -9,12 +9,12 @@ function HomePage() {
       <Card>
         <h2 className="HomePage-title">Welcome to the salad shop</h2>
         <p className="HomePage-subtitle">
-          This is the right place to buy salad!
+          This is the right place to buy a salad!
         </p>
         <span role="img" aria-label="salad" className="HomePage__image">
           🥗
         </span>
-        <Button to="ingredients">Start now</Button>
+        <Button to="ingredients">Order Salad</Button>
       </Card>
     </div>
   );
